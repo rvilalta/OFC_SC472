501c377ffaa2e3cbf1a85cc65db3b9abfe8cd13c
lrodrin <lrodrin@gmail.com>
2018-11-21 15:34:09 +0100

    Work on grpc

---
+++ grpc/connection/create.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+import connection_pb2
+import sys
+
+try:
+  raw_input          # Python 2
+except NameError:
+  raw_input = input  # Python 3
+
+
+# This function fills in a Connection message based on user input.
+def PromptForConnection(connection):
+  connection.connectionId = raw_input("Enter connectionID: ")
+  connection.sourceNode = raw_input("Enter sourceNode: ")  
+  connection.targetNode = raw_input("Enter targetNode: ")
+  connection.sourcePort = raw_input("Enter sourcePort: ")  
+  connection.targetPort = raw_input("Enter targetPort: ") 
+  connection.bandwidth = int( raw_input("Enter bandwidth: ") )
+  
+  type = raw_input("Is this a eth or optical connection? ")
+  if type == "eth":
+    connection.layerProtocolName = connection_pb2.Connection.ETH
+  elif type == "optical":
+    connection.layerProtocolName = connection_pb2.Connection.OPTICAL
+  else:
+    print("Unknown layerProtocolName type; leaving as default value.")
+
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print("Usage:", sys.argv[0], "CONNECTION_FILE")
+    sys.exit(-1)
+  
+  connectionList = connection_pb2.ConnectionList()
+  
+  # Read the existing address book.
+  try:
+    with open(sys.argv[1], "rb") as f:
+      connectionList.ParseFromString(f.read())
+  except IOError:
+    print(sys.argv[1] + ": File not found.  Creating a new file.")
+  
+  # Add an address.
+  PromptForConnection(connectionList.connection.add())
+  
+  # Write the new address book back to disk.
+  with open(sys.argv[1], "wb") as f:
+    f.write(connectionList.SerializeToString())
---
+++ grpc/connection/list.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+from __future__ import print_function
+import connection_pb2
+import sys
+
+
+# Iterates though all connections in the ConnectionList and prints info about them.
+def ListConnections(connectionList):
+  for connection in connectionList.connection:
+    print("connectionID:", connection.connectionId)
+    print("  sourceNode:", connection.sourceNode)
+    print("  targetNode:", connection.targetNode)
+    print("  sourcePort:", connection.sourcePort)  
+    print("  targetPort:", connection.targetPort)
+    print("  bandwidth:", connection.bandwidth)       
+    if connection.layerProtocolName == connection_pb2.Connection.ETH:
+      print("  layerProtocolName:ETH")
+    elif connection.layerProtocolName == connection_pb2.Connection.OPTICAL:
+      print("  layerProtocolName:OPTICAL")
+
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print("Usage:", sys.argv[0], "CONNECTION_FILE")
+    sys.exit(-1)
+  
+  connectionList = connection_pb2.ConnectionList()
+  
+  # Read the existing address book.
+  with open(sys.argv[1], "rb") as f:
+    connectionList.ParseFromString(f.read())
+  
+  ListConnections(connectionList)
